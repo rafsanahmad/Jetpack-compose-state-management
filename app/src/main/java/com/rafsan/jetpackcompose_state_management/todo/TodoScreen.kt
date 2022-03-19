8807eed51dc9cab2d48614efdcda5a47daaa2b47
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-03-19 18:28:18 +0600

    Todo Screen

---
+++ app/src/main/java/com/rafsan/jetpackcompose_state_management/todo/TodoScreen.kt
@@ -0,0 +1,102 @@
+package com.rafsan.jetpackcompose_state_management.todo
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rafsan.jetpackcompose_state_management.util.generateRandomTodoItem
+import kotlin.random.Random
+
+/**
+ * Stateless component that is responsible for the entire todo screen.
+ *
+ * @param items (state) list of [TodoItem] to display
+ * @param onAddItem (event) request an item be added
+ * @param onRemoveItem (event) request an item be removed
+ */
+@Composable
+fun TodoScreen(
+    items: List<TodoItem>,
+    onAddItem: (TodoItem) -> Unit,
+    onRemoveItem: (TodoItem) -> Unit
+) {
+    Column {
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(top = 8.dp)
+        ) {
+            items(items = items) {
+                TodoRow(
+                    todo = it,
+                    onItemClicked = { onRemoveItem(it) },
+                    modifier = Modifier.fillParentMaxWidth()
+                )
+            }
+        }
+
+        // For quick testing, a random item generator button
+        Button(
+            onClick = { onAddItem(generateRandomTodoItem()) },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        ) {
+            Text("Add random item")
+        }
+    }
+}
+
+/**
+ * Stateless composable that displays a full-width [TodoItem].
+ *
+ * @param todo item to show
+ * @param onItemClicked (event) notify caller that the row was clicked
+ * @param modifier modifier for this element
+ */
+@Composable
+fun TodoRow(todo: TodoItem, onItemClicked: (TodoItem) -> Unit, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .clickable { onItemClicked(todo) }
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(todo.task)
+        Icon(
+            imageVector = todo.icon.imageVector,
+            contentDescription = stringResource(id = todo.icon.contentDescription)
+        )
+    }
+}
+
+private fun randomTint(): Float {
+    return Random.nextFloat().coerceIn(0.3f, 0.9f)
+}
+
+@Preview
+@Composable
+fun PreviewTodoScreen() {
+    val items = listOf(
+        TodoItem("Learn compose", TodoIcon.Event),
+        TodoItem("Take the codelab"),
+        TodoItem("Apply state", TodoIcon.Done),
+        TodoItem("Build dynamic UIs", TodoIcon.Square)
+    )
+    TodoScreen(items, {}, {})
+}
+
+@Preview
+@Composable
+fun PreviewTodoRow() {
+    val todo = remember { generateRandomTodoItem() }
+    TodoRow(todo = todo, onItemClicked = {}, modifier = Modifier.fillMaxWidth())
+}
