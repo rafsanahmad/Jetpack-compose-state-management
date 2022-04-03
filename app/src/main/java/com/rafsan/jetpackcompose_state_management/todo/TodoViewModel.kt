b347783824a34d082f83d05f8e7754757ead5b67
Rafsan Ahmad <rafsanahmad111@gmail.com>
2022-04-03 23:55:04 +0600

    using state in viewmodel

--- app/src/main/java/com/rafsan/jetpackcompose_state_management/todo/TodoViewModel.kt
+++ app/src/main/java/com/rafsan/jetpackcompose_state_management/todo/TodoViewModel.kt
@@ -1,15 +1,22 @@
 package com.rafsan.jetpackcompose_state_management.todo
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
 
+    //private state
+    private var currentEditPosition by mutableStateOf(-1)
+
     // state: todoItems
     var todoItems = mutableStateListOf<TodoItem>()
         private set
+
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     // event: addItem
     fun addItem(item: TodoItem) {
@@ -19,5 +26,26 @@ class TodoViewModel : ViewModel() {
     // event: removeItem
     fun removeItem(item: TodoItem) {
         todoItems.remove(item)
+        onEditDone() // don't keep the editor open when removing items
+    }
+
+    // event: onEditItemSelected
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    // event: onEditDone
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    // event: onEditItemChange
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
+        }
+
+        todoItems[currentEditPosition] = item
     }
 }
